bf8a3d80eada2a854236b637b580c75b476a6452
Harshit Sharma <brown.hash@outlook.com>
2022-01-27 23:56:14 +0530

    feat: Kang homebrew formula

--- formula/kang.rb
+++ formula/kang.rb
@@ -1,18 +1,14 @@
-require "formula"
-
-class kang < Formula
+class Kang < Formula
   desc "Use endless terraform versions in different shell sessions independently"
   homepage "https://github.com/brownhash/kang"
-  url "https://github.com/brownhash/kang/releases/download/v1.0.0-beta/kang_darwin_amd64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "389b9858d799e03e65fe6cdb6f62e0c28ef0b2e4c1794c1b62e7e47b9809c7c0"
-  head "https://github.com/brownhash/kang.git"
+  version "1.0.0-beta"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/brownhash/kang/releases/download/v1.0.0-beta/kang_darwin_amd64.tar.gz"
+    sha256 "fa49007fc8c3fef09c023352c3703469422e3e4d87e252142135c4617155bf43"
+  end
 
   def install
     bin.install "kang"
-  end
-
-  # Homebrew requires tests.
-  test do
-    assert_match "1.0.0-beta", shell_output("#{bin}/kang -v", 2)
   end
 end
